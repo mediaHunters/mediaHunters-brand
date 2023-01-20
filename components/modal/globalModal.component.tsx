0541f05cf69005b96464ef7f4f52b66e40cedc53
mediahunters <hello.mediahunters@gmail.com>
2023-01-20 17:56:11 +0100

    feat(projects): add modal for projects section (MHP-5)

--- components/modal/globalModal.component.tsx
+++ components/modal/globalModal.component.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState, createContext, useContext } from "react";
 import ProjectModal from "./project-modal";
