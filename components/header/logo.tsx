6f15b86eda195af2de68b5a56fbe893fe613667b
mediahunters <hello.mediahunters@gmail.com>
2023-02-12 17:00:23 +0100

    add anotehr posts

--- components/header/logo.tsx
+++ components/header/logo.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import styled from "styled-components";
 
 import React from "react";
@@ -11,15 +12,17 @@ const LogoWrapper = styled.div`
 const Logo = () => {
   return (
     <LogoWrapper>
-      <Image
-        alt="tworzenie-i-projektowanie-stron"
-        src="/images/MediaHunters.png"
-        width="125"
-        height="125"
-        priority={true}
-        loader={({ src }: { src: string }) => src}
-      />
+      <Link href={"/"}>
+        <Image
+          alt="tworzenie-i-projektowanie-stron"
+          src="/images/MediaHunters.png"
+          width="125"
+          height="125"
+          priority={true}
+          loader={({ src }: { src: string }) => src}
+        />
+      </Link>
     </LogoWrapper>
   );
 };
-export default Logo
+export default Logo;
